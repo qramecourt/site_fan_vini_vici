d554074556f1c7243761898b0cdc38e41c97bfcf
Quentin Ramecourt <pro.quentin.ramecourt@gmail.com>
2022-08-18 20:58:19 +0200

    avancée dans le projet

--- front/app.js
+++ front/app.js
@@ -1,1 +1,19 @@
-const getCountries = document.getElementById('select');
+const getCountriesSelect = document.getElementById('select')
+//fetch to feed the dropdown in the form
+const fetchURLCountriesAPI = 'https://restcountries.com/#api-endpoints-v3-name';
+//cont to get the main dates for the group
+const getDebutDate = document.getElementById('')
+function getCountries() {
+    fetch(fetchURLCountriesAPI, {
+        method: 'GET',
+    })
+    .then(function (response){
+        return response.json()
+    })
+    .then(function(responseJSON){
+        
+    });
+}
+
+
+getCountriesSelect.addEventListener('click', getCountries)
